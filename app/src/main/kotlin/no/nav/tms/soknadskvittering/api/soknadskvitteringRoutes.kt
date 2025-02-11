8e343601abd1f32d8c647469813bd34828eec172
Christopher Santana <christopher.santana@nav.no>
2025-02-11 15:31:52 +0100

    Minifiks

--- app/src/main/kotlin/no/nav/tms/soknadskvittering/api/soknadskvitteringRoutes.kt
+++ app/src/main/kotlin/no/nav/tms/soknadskvittering/api/soknadskvitteringRoutes.kt
@@ -5,7 +5,6 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import no.nav.tms.soknadskvittering.aggregation.SoknadsKvitteringRepository
-import no.nav.tms.soknadskvittering.user
 import no.nav.tms.soknadskvittering.userIdent
 
 private const val soknadsIdParameter = "soknadsId"
